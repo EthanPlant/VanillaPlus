0e5b9ea0a67651ed28894b688d5cb3fb0556c0d9
Ethan Plant <plant.ethan@gmail.com>
2022-07-05 07:13:28 -0400

    Initial commit

---
+++ src/main/java/com/aquilla/vanillaplus/mixin/VanillaPlusMixin.java
@@ -0,0 +1,16 @@
+package com.aquilla.vanillaplus.mixin;
+
+import com.aquilla.vanillaplus.VanillaPlus;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class VanillaPlusMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        VanillaPlus.LOGGER.info("This line is printed by a Vanilla+ mixin!");
+    }
+}
